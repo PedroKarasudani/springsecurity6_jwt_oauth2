05e583c01170a26823df848ac7a8ec3fd96603d7
Pedro Karasudani Desktop <pedro.karasudani@hotmail.com>
2025-04-15 23:06:32 -0300

    Criando os repositories das entities

---
+++ src/main/java/tech/buildrun/springsecurity/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package tech.buildrun.springsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.buildrun.springsecurity.entities.Role;
+import tech.buildrun.springsecurity.entities.Tweet;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
